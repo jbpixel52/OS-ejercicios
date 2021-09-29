1dee19c9c8bc3fe6da610b41c494e2dcf9cc76ba
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2021-09-28 19:41:37 -0700

    Create threads._sleep.py

---
+++ threads._sleep.py
@@ -0,0 +1,12 @@
+#%%
+import time
+from threading import Thread
+
+def sleeper(i):
+    print("Thread {0} sleeps for 5 seconds".format(i))
+    time.sleep(5)
+    print("Thread {0} woke up".format(i))
+    
+for i in range(10):
+    t =Thread(target=sleeper, args=(i,))
+    t.start()
